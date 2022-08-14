5ad451b0dd69b97de90b62db2c6c336d28e7f891
Paul <64870518+woodcock3@users.noreply.github.com>
2022-08-14 09:38:46 +0100

    Create meta.js

---
+++ src/_data/meta.js
@@ -0,0 +1,12 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  environment: process.env.ELEVENTY_ENV,
+  // Author
+  author: "Rusticola",
+  url: "https://rusticola.co.uk",
+
+  // Contact us
+  email: "",
+  phone: "",
+  ariaPhone: "phone number in words",
+};
