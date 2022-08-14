324c0a7faaace78d2ff3f8c7875ac5cb9528b7d9
Paul <64870518+woodcock3@users.noreply.github.com>
2022-08-14 09:45:33 +0100

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,6 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   environment: process.env.ELEVENTY_ENV,
   // Author
+  name: "Bonsai css",
   author: "Rusticola",
   url: "https://rusticola.co.uk",
 
