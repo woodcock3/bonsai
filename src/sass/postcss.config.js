c6a831472d24232dc47f18470f29b398630bb277
Paul <64870518+woodcock3@users.noreply.github.com>
2022-08-14 05:47:34 +0100

    Update postcss.config.js

--- src/sass/postcss.config.js
+++ src/sass/postcss.config.js
@@ -1,5 +1,12 @@
 // postcss.config.js
 
+const postcssImport = require('postcss-import');
+const postcssInlineSvg = require('postcss-inline-svg');
+const postcssSimpleVars = require('postcss-simple-vars');
+const postcssNested = require('postcss-nested');
+const postcssMixins = require('postcss-mixins');
+const postcssCombineMediaQuery = require('postcss-combine-media-query');
+
 const autoprefixer = require('autoprefixer');
 const cssnanoConfig = {
     autoprefixer: false,
@@ -13,15 +20,18 @@ const cssnano = require('cssnano')({
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./**/*.html'],
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  css: ['city.css'],
-  safelist: {
-    standard: ['::-webkit-scrollbar', '::-webkit-scrollbar-thumb', '::-webkit-scroll-track', 'gradient-text'],
-    greedy: [/^bg-/]
-  }
+  css: ['bansai.css'],
+  safelist: {}
 });
 
 module.exports = {
   plugins: [
+    postcssImport,
+    postcssInlineSvg, 
+    postcssMixins, 
+    postcssSimpleVars, 
+    postcssNested, 
+    postcssCombineMediaQuery,
     autoprefixer,
     ...(process.env.NODE_ENV === "production" ? [cssnano, purgecss] : [])
   ],
